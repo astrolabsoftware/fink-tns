c5a55ea25faeb18c02801f88698fbfe066d3cf2d
JulienPeloton <peloton@lal.in2p3.fr>
2025-01-04 10:50:30 +0100

    [hotfix] fix parameters for submission. Bump to 0.13

--- fink_tns/report.py
+++ fink_tns/report.py
@@ -371,6 +371,6 @@ def send_json_report(api_key, url, json_file_path, tns_marker) -> int:
     headers = {'User-Agent': tns_marker}
 
     # send json report using request module
-    response = requests.post(json_url, files=json_data, headers=headers)
+    response = requests.post(json_url, data=json_data, headers=headers)
 
     return response
