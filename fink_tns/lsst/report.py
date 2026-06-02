0940527107d3f7ada52594dc0b01423c3c986cbb
JulienPeloton <peloton@lal.in2p3.fr>
2026-06-02 14:13:34 +0200

    Switch to archives for non-detection

--- fink_tns/lsst/report.py
+++ fink_tns/lsst/report.py
@@ -171,7 +171,7 @@ def extract_discovery_photometry_api(data: pd.DataFrame) -> (dict, dict):
     }
 
 
-    last_non_detection = {}
+    last_non_detection = {"archiveid": "2"}
 
     return first_photometry, last_non_detection
 
@@ -227,7 +227,11 @@ def build_report(
         "internal_name": "LSST-AP-DO-{}".format(data["diaObject"].apply(lambda x: x['diaObjectId']).values[0]),
         "remarks": remarks_custom.format(data["diaObject"].apply(lambda x: x['diaObjectId']).values[0]),
         "non_detection": non_detection,
-        "photometry": {"photometry_group": {'0': photometry}}
+        "photometry": {"photometry_group": {'0': photometry}},
+        "archives": {
+            "0": "Other",
+            "1": "SDSS",
+        },
     }
 
     return report
@@ -287,11 +291,15 @@ def build_report_api(
         "discovery_data_source_id": discovery_data_source_id,
         "reporter": reporter_custom,
         "discovery_datetime": photometry['obsdate'],
-        "at_type": at_type,
+        "at_type": at_type_,
         "internal_name": "LSST-AP-DO-{}".format(data['r:diaObjectId'].values[0]),
         "remarks": remarks_custom.format(data['r:diaObjectId'].values[0]),
         "non_detection": non_detection,
-        "photometry": {"photometry_group": {'0': photometry}}
+        "photometry": {"photometry_group": {'0': photometry}},
+        "archives": {
+            "0": "Other",
+            "1": "SDSS",
+        },
     }
 
     return report
